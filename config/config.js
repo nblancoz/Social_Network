8c3af989f7fe9e73d6fc1c0d6132f38f5225f9cf
nblancoz <nblancozappala@gmail.com>
2023-11-10 10:35:00 +0100

    Database connected successfully

---
+++ config/config.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+const { MONGO_URI } = require("./keys");
+
+const dbConnection = async () => {
+  try {
+    await mongoose.connect(MONGO_URI);
+    console.log("Database connected succesfully");
+  } catch (error) {
+    console.error(error);
+    throw new Error("Error connecting the Database");
+  }
+};
+
+module.exports = { dbConnection };
