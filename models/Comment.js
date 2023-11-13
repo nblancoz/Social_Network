802437163a50d25e1aad31110aba18cea3a1067b
nblancoz <nblancozappala@gmail.com>
2023-11-13 14:50:07 +0100

    added to develop

---
+++ models/Comment.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+const CommentSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Comment = mongoose.model("Comment", CommentSchema);
+
+module.exports = Comment;
