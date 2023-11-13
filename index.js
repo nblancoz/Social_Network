f7c26169a41503709481c3b76f6874a62359c1e3
Luciana Boasso <luboasso16@gmail.com>
2023-11-13 15:27:28 +0100

    Post creaete

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const { dbConnection } = require("./config/config");
 app.use(express.json());
 
 app.use("/users", require("./routes/users.js"));
+app.use("/posts", require("./routes/posts.js"));
 
 dbConnection();
 
