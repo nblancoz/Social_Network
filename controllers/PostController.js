ffc125558a57294f34c9c57cfa8d709cd6f630fa
Luciana Boasso <luboasso16@gmail.com>
2023-11-13 16:40:41 +0100

    update completed

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -4,14 +4,28 @@ const PostController = {
         async create(req, res) {
           try {
             const post = await Post.create({
-              ...req.body,
-              // userId: req.user._id, no funciona
+              ...req.body
             });
             res.status(201).send(post);
           } catch (error) {
             console.error(error);
           }
         },
+
+        async update(req, res) {
+            try {
+              const post = await Post.findByIdAndUpdate(
+                req.params._id,
+                { ...req.body},
+                {
+                  new: true,
+                }
+              );
+              res.send({ message: "Post successfully updated", post });
+            } catch (error) {
+              console.error(error);
+            }
+          },
     }
 
 module.exports = PostController;
