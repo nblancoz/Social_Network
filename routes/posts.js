d9805a8039c242a66505cf98b4ad4c8235aa7472
nblancoz <nblancozappala@gmail.com>
2023-11-14 10:16:57 +0100

    added authentication for create post

--- routes/posts.js
+++ routes/posts.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const PostController = require("../controllers/PostController");
+const { authentication } = require("../middlewares/authentication");
 const router = express.Router();
 
-router.post("/", PostController.create);
+router.post("/", authentication, PostController.create);
 router.put("/id/:_id", PostController.update);
 
 module.exports = router;
