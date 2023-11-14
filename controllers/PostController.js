fc93be1e1759fbc293a07c5482589dd3b55a2dad
nblancoz <nblancozappala@gmail.com>
2023-11-14 10:02:24 +0100

    create post working

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -3,9 +3,7 @@ const Post = require("../models/Post");
 const PostController = {
         async create(req, res) {
           try {
-            const post = await Post.create({
-              ...req.body
-            });
+            const post = await Post.create(req.body);
             res.status(201).send(post);
           } catch (error) {
             console.error(error);
