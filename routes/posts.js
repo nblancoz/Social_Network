f42825ba52c6bebca387391dd7b486c8301e6be2
nblancoz <nblancozappala@gmail.com>
2023-11-14 15:27:29 +0100

    added isAdmin & isAuthor

--- routes/posts.js
+++ routes/posts.js
@@ -4,6 +4,6 @@ const { authentication } = require("../middlewares/authentication");
 const router = express.Router();
 
 router.post("/", authentication, PostController.create);
-router.put("/id/:_id", PostController.update);
+router.put("/id/:_id", authentication, PostController.update);
 
 module.exports = router;
