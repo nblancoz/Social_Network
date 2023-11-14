37bd5a09d0b415238ec57124e13c1ec477aa8348
nblancoz <nblancozappala@gmail.com>
2023-11-14 16:02:51 +0100

    create post working

--- routes/posts.js
+++ routes/posts.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const PostController = require("../controllers/PostController");
-// const { authentication } = require("../middlewares/authentication");
+const { authentication } = require("../middlewares/authentication");
 const router = express.Router();
 
 router.post("/", authentication, PostController.create);
